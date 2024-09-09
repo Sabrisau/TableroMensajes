33f88b296fff5f4768f3b0439adf079e47f5a904
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-09-08 22:28:11 -0300

    Defino views.py

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MensajeForm
 
-# Create your views here.
+def crear_mensaje(request):
+    if request.method == 'POST':
+        form = MensajeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ver_mensajes_enviados')  # Redirigir a otra vista
+    else:
+        form = MensajeForm()
+    return render(request, 'mensajes/crear_mensaje.html', {'form': form})
