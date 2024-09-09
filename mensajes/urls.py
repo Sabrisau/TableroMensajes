ee2078aa7363c379dd4b4989075857e4962337c7
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-09-08 23:04:32 -0300

    Defino models.py

--- mensajes/urls.py
+++ mensajes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import crear_mensaje
+from mensajes import views
 
 urlpatterns = [
-    path('crear/', crear_mensaje, name='crear_mensaje'),
-    path('recibidos/', ver_mensajes_recibidos, name='ver_mensajes_recibidos'),
-    path('enviados/', ver_mensajes_enviados, name='ver_mensajes_enviados'),
-    path('eliminar/<int:id>/', eliminar_mensajes, name='eliminar_mensaje'),
+    path('crear/', views.crear_mensaje_view, name='crear_mensaje'),
+    path('recibidos/', views.ver_mensajes_recibidos_view, name='ver_mensajes_recibidos'),
+    path('enviados/', views.ver_mensajes_enviados_view, name='ver_mensajes_enviados'),
+    path('eliminar/<int:id>/', views.eliminar_mensajes_views, name='eliminar_mensaje'),
 ]
--- mensajes/views.py
+++ mensajes/views.py
@@ -3,7 +3,7 @@ from .forms import MensajeForm
 from django.shortcuts import get_object_or_404
 from .models import Mensaje
 
-def crear_mensaje(request):
+def crear_mensaje_view(request):
     if request.method == 'POST':
         form = MensajeForm(request.POST)
         if form.is_valid():
@@ -13,15 +13,15 @@ def crear_mensaje(request):
         form = MensajeForm()
     return render(request, 'mensajes/crear_mensaje.html', {'form': form})
 
-def ver_mensajes_recibidos(request):
+def ver_mensajes_recibidos_view(request):
     mensajes = Mensaje.objects.filter(destinatario=request.user.username)
     return render(request, 'mensajes/mensajes_recibidos.html', {'mensajes': mensajes})
 
-def ver_mensajes_enviados(request):
+def ver_mensajes_enviados_views(request):
     mensajes = Mensaje.objects.filter(remitente=request.user.username)
     return render(request, 'mensajes/mensajes_enviados.html', {'mensajes': mensajes})
 
-def eliminar_mensaje(request, id):
+def eliminar_mensaje_views(request, id):
     mensaje = get_object_or_404(Mensaje, id=id)
     if request.method == 'POST':
         mensaje.delete()
